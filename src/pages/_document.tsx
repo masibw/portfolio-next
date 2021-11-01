fbd0a6e0187b80d8e0fb28cbf105513c6e252898
masimacbook <masi19bw@gmail.com>
2021-11-01 12:11:17 +0900

    feat: add icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,31 @@ class Document extends NextDocument<Props> {
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@masibw" />
           <meta property="og:locale" content="ja_JP" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
